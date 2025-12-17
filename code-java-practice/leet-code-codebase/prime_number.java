8131cbe2f0e4330c24b157359e98cb0e30ef5dc5
sahil9977 <factstar1290@gmail.com>
2025-12-17 14:57:18 +0530

    [Sahil] java problems

---
+++ code-java-practice/leet-code-codebase/prime_number.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class prime_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean check = true;
+
+        if (n <= 1) {
+            check = false;
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    check = false;
+                    break;
+                }
+            }
+        }
+
+        if (check)
+            System.out.println(n + "Prime Number");
+        else
+            System.out.println(n + "not Prime Number");
+    }
+}
