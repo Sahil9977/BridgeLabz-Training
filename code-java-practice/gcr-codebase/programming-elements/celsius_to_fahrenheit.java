c6a3bcb8a3a41c15dabdb2c6c1ddf36b25f40f0a
sahil9977 <factstar1290@gmail.com>
2025-12-18 16:33:17 +0530

    [Sahil] Level 1 and Level 2 Solved

--- code-java-practice/gcr-codebase/programming-elements/celsius_to_fahrenheit.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class celsius_to_fahrenheit {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int c = sc.nextInt();
-        int f = (c * 9/5) + 32;
-        System.out.println(f);
-    }
-}
