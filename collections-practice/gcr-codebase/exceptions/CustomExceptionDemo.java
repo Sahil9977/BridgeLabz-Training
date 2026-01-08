bc023f188368463aaa7b1903af3d5392f281ded7
Sahil verma <118045929+Sahil9977@users.noreply.github.com>
2026-01-08 16:38:20 +0530

    [Sahil Verma] Add . Solved exception problems

--- collections-practice/gcr-codebase/exceptions/CustomExceptionDemo.java
+++ collections-practice/gcr-codebase/exceptions/CustomExceptionDemo.java
@@ -1,7 +1,6 @@
 package exceptions;
 import java.util.Scanner;
 
-// Custom Exception Class
 class InvalidAgeException extends Exception {
     public InvalidAgeException(String message) {
         super(message);
@@ -9,13 +8,11 @@ class InvalidAgeException extends Exception {
 }
 
 public class CustomExceptionDemo {
-
-    // Method to validate age
     static void validateAge(int age) throws InvalidAgeException {
         if (age < 18) {
             throw new InvalidAgeException("Age must be 18 or above");
         } else {
-            System.out.println("✅ Access granted!");
+            System.out.println("Access granted!");
         }
     }
 
@@ -25,12 +22,10 @@ public class CustomExceptionDemo {
         try {
             System.out.print("Enter your age: ");
             int age = scanner.nextInt();
-
-            // Call validation method
             validateAge(age);
 
         } catch (InvalidAgeException e) {
-            System.out.println("⚠️ Exception: " + e.getMessage());
+            System.out.println( e.getMessage());
         } finally {
             scanner.close();
             System.out.println("Program ended.");
