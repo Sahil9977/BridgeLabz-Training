14b8a58c832f590b41677c67049a59172a7cea34
Sahil verma <118045929+Sahil9977@users.noreply.github.com>
2026-01-08 16:45:01 +0530

    Update ThrowThrowsDemo.java

--- collections-practice/gcr-codebase/exceptions/ThrowThrowsDemo.java
+++ collections-practice/gcr-codebase/exceptions/ThrowThrowsDemo.java
@@ -1,10 +1,7 @@
 package exceptions;
 public class ThrowThrowsDemo {
-
-    // Method that propagates exception using 'throws'
     public static double calculateInterest(double amount, double rate, int years) throws IllegalArgumentException {
         if (amount < 0 || rate < 0) {
-            // 'throw' is used to explicitly throw an exception
             throw new IllegalArgumentException("Amount and rate must be positive");
         }
         return (amount * rate * years) / 100;
@@ -12,15 +9,13 @@ public class ThrowThrowsDemo {
 
     public static void main(String[] args) {
         try {
-            double interest = calculateInterest(10000, 5, 2); // valid input
-            System.out.println("✅ Calculated Interest: " + interest);
-
-            // Invalid case: negative amount
+            double interest = calculateInterest(10000, 5, 2); 
+            System.out.println("interest);
             double invalidInterest = calculateInterest(-5000, 5, 2);
             System.out.println("Interest: " + invalidInterest);
 
         } catch (IllegalArgumentException e) {
-            System.out.println("⚠️ Invalid input: " + e.getMessage());
+            System.out.println( e.getMessage());
         }
     }
 }
