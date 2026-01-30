76d1cc972fbcb488ca6b1a15d3a5c3741ee21b2e
sahil9977 <factstar1290@gmail.com>
2026-01-30 12:02:23 +0530

    [Sahil Verma] Refactor . Solved Flip Key Logical Problem

--- core-java-practice/scenario-based/flip-key-logical-program/Program.java
+++ core-java-practice/scenario-based/flip-key-logical-program/Program.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class FlipKeyLogicalProblem {
+public class Program {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
