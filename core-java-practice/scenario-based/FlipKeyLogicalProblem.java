7bc176978734ab0d8f359e4f8f79e4688eaf61e4
sahil9977 <factstar1290@gmail.com>
2026-01-30 11:57:23 +0530

    [Sahil Verma] Add . Solved Flip Key Logical Problem

---
+++ core-java-practice/scenario-based/FlipKeyLogicalProblem.java
@@ -0,0 +1,46 @@
+package scnerio_based;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FlipKeyLogicalProblem {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the word");
+		String input = sc.nextLine();
+		
+		if(CleanseAndInvent(input).length()<1)System.out.println("Invalid Input");
+		else System.out.println(CleanseAndInvent(input));
+		
+		
+	}
+	
+	public static  String CleanseAndInvent(String input) {
+		if(input.length()<6 || input.equals(null))return "";
+		String regex = "[^a-zA-Z]";
+		Matcher matcher= Pattern.compile(regex).matcher(input);
+
+		if(input.contains(" ") || matcher.find())return "";
+		
+		String temp="";
+		
+		for(int i=0;i<input.length();i++) {
+			if(input.toLowerCase().charAt(i)%2!=0)temp+=input.charAt(i);
+		}
+		
+		String updatedInput = new StringBuilder(temp).reverse().toString();
+	
+		String result ="";
+		for(int i=0;i<updatedInput.length();i++) {
+			if(i%2==0) result+=Character.toUpperCase(updatedInput.charAt(i));
+			else result+=updatedInput.charAt(i);
+		}
+		
+		return result;
+		
+	}
+
+}
