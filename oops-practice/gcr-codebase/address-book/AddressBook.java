4c366d8b373eaf82bdfa5918ffddc7c290ab2ce1
Sahil Verma <braj91893@gmail.com>
2026-02-23 11:46:56 +0530

    [Sahil Verma] Add . Solved the Address Book problem till UC8

--- oops-practice/gcr-codebase/address-book/AddressBook.java
+++ oops-practice/gcr-codebase/address-book/AddressBook.java
@@ -3,12 +3,22 @@ package address_book;
 import java.util.*;
 
 public class AddressBook {
+
 	
-	public  static void addNewContact(Scanner sc , List<Contact> addressBook) {
+	List<Contact> addressBook = new ArrayList<>();
+	
+	public boolean addNewContact(Scanner sc ) {
 		
 		
-		System.out.println("Enter the first name of person :\n");
+		System.out.println("\nEnter the first name of person :\n");
 		String fName = sc.nextLine();
+		
+//		for(int i=0;i<addressBook.size();i++) {
+//			if(addressBook.get(i).getFirstName().equalsIgnoreCase(fName)) {
+//				System.out.println("Duplicate name found");
+//				return false;
+//			};
+//		}
 		
 		System.out.println("Enter the last name of person :\n");
 		String lName = sc.nextLine();
@@ -24,17 +34,120 @@ public class AddressBook {
 		
 		System.out.println("Enter the zip code of person :\n");
 		int zip = sc.nextInt();
+		sc.nextLine();
 		
 		System.out.println("Enter the phone number of person :\n");
 		int phoneNumber = sc.nextInt();
+		sc.nextLine();
 		
 		System.out.println("Enter the email of person :\n");
 		String email = sc.nextLine();
 		
-		addressBook.add(new Contact(fName, lName, address, city, state, zip, phoneNumber, email ));
+		if(addressBook.contains(new Contact(fName, lName, address, city, state, zip, phoneNumber, email ))) {
+			System.out.println("\nDuplicate found.");
+			return false;
+		}
+		
+		// why we dont direct use .equals
+		// because .contains internally use .equals || we can use .equals directly but for that we make a loop then we have to .equals like that
+//		for(Contact p : list) {
+//		    if(p.equals(newPerson)) {
+//		        // duplicate
+//		    }
+//		}
+
+		
+		//implementation on .contains
+//		ArrayList<Person> list = new ArrayList<>();
+//		list.contains(new Person("Aman"));
+//		for(Person p : list) {
+//		    if(p.equals(newPerson)) {
+//		        return true;
+//		    }
+//		}
+
+		else return addressBook.add(new Contact(fName, lName, address, city, state, zip, phoneNumber, email ));
 	}
 	
+	public boolean editContact(Scanner sc ) {
+		
+		System.out.println("Enter the name of person you want to edit :/n");
+		String fName = sc.nextLine();
+		
 
+		if(addressBook.size()==0) return false;
+		else {
+			for(int i=0;i<addressBook.size();i++) {
+				if(addressBook.get(i).getFirstName().equalsIgnoreCase(fName)) {
+					System.out.println("Enter updated first name :/n");
+					String updatedFName = sc.nextLine();
+					
+					if(addressBook.get(i).getFirstName().equalsIgnoreCase(fName)) {
+						System.out.println("Duplicate name found");
+						return false;
+					};
+					
+					System.out.println("Enter updated last name :/n");
+					String updatedLName = sc.nextLine();
+					
+					System.out.println("Enter updated address :/n");
+					String updatedAddress = sc.nextLine();
+					
+					System.out.println("Enter updated city :/n");
+					String updatedCity = sc.nextLine();
+					
+					System.out.println("Enter updated state :/n");
+					String updatedState = sc.nextLine();
+					
+					System.out.println("Enter updated zip code :/n");
+					int updatedZipCode = sc.nextInt();
+					
+					System.out.println("Enter updated phone number :/n");
+					int updatedPhoneNumber= sc.nextInt();
+					
+					System.out.println("Enter updated email :/n");
+					String updatedEmail = sc.nextLine();
+					
+				
+					addressBook.get(i).setFirstName(updatedFName) ;
+					addressBook.get(i).setLastName ( updatedLName);
+					addressBook.get(i).setAddress(updatedAddress);
+					addressBook.get(i).setCity(updatedCity);
+					addressBook.get(i).setState(updatedState);
+					addressBook.get(i).setZipCode(updatedZipCode);
+					addressBook.get(i).setPhoneNumber(updatedPhoneNumber);
+					addressBook.get(i).setEmail(updatedEmail);
+					return true;
+				}
+				
+			}
+			return false;
+		}
+
+	}
+	
+	public Contact searchContactByCityOrState( ) {
+		 return null;
+	}
+	
+	public boolean deleteContact(Scanner sc ) {
+		
+		System.out.println("Enter the Name of person you want to delete : ");
+		String fName = sc.nextLine();
+		
+		if(addressBook.size()<=0)return false;
+		else {
+			for(int i=0;i<addressBook.size() ;i++) {
+				if(addressBook.get(i).getFirstName().equalsIgnoreCase(fName)) {
+					 addressBook.remove(i);
+					 return true;
+				}
+				
+			}
+			return false;
+		}
+	}
+	
 	
 	
 	
